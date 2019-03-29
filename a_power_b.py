8e218b7ed57888dc8aa5567f57ed1e1d32ce39c3
checho <sergio.baron.2019@upb.edu.co>
2019-03-29 11:25:58 -0500

    mi primera modificacion

--- a_power_b.py
+++ a_power_b.py
@@ -1,12 +1,17 @@
-def a_power_b(a, b):
-    prod = 1
-    for x in range(1, b + 1):
-        prod = prod * a
 
-    print(prod)
+while True:
+    a = int(input("ingrese la base del numero:"))
+    if a==0:
+        print("final del programa")
+        break
 
 
-a= int(input("ingrese la base del numero:"))
-b=int(input("ingrese el exponente del numero: "))
+    b = int(input("ingrese el exponente del numero: "))
+    def a_power_b(a, b):
+        prod = 1
+        for x in range(1, b + 1):
+            prod = prod * a
 
-a_power_b(a , b )
+        print(prod)
+
+    a_power_b(a , b )
