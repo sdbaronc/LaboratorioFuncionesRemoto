5db5d701ef709751e9d71313f0750e5d084a2b64
checho <sergio.baron.2019@upb.edu.co>
2019-03-30 10:11:10 -0500

    primera modifiacion segundo ejercicio

--- is_prime.py
+++ is_prime.py
@@ -1,11 +1,22 @@
-n=int(input("ingrese un numero :"))
-def is_prime(n):
-    cont=0
-    for x in range(1,n+1):
-        if n%x==0:
-            cont+=1
-    if cont>2:
-        print("Is NOT a prime number")
-    else:
-        print("Is a prime number")
-is_prime(n)        
+while True:
+    n = int(input("ingrese un numero :"))
+    if n <=0:
+        print("finish")
+        break
+
+    def is_prime():
+        try:
+            cont=0
+            for x in range(1,n+1):
+                if n%x==0:
+                    cont+=1
+            if cont==2:
+                print("Is a prime number")
+                return 1
+            else:
+                print("Is NOT a prime number")
+                return 0
+        except ValueError :
+            print("hubo un eror")
+            return -1
+    is_prime()
