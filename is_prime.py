3afdaef566e98be7466c0d26dfb16cf4e86c5288
checho <sergio.baron.2019@upb.edu.co>
2019-03-30 16:11:56 -0500

    segunda modificaion segundo ejercicio

--- is_prime.py
+++ is_prime.py
@@ -1,8 +1,17 @@
+prim=0
 while True:
     n = int(input("ingrese un numero :"))
     if n <=0:
         print("finish")
         break
+    contp=0
+    for x in range(1, n + 1):
+        if n % x == 0:
+            contp += 1
+    if contp == 2:
+        prim+=1
+
+
 
     def is_prime():
         try:
@@ -16,7 +25,19 @@ while True:
             else:
                 print("Is NOT a prime number")
                 return 0
+
+
         except ValueError :
             print("hubo un eror")
             return -1
+
     is_prime()
+print("la cantidad d numeros primos es :" ,prim)
+con2=0
+for x in range(1,n+1):
+        if n%x==0:
+            con2+=1
+if con2>2:
+    print("la cantidad de numeros primos no es prima")
+else:
+    print("la cantidad de numeros primos es prima")
